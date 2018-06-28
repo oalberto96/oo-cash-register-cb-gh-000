55a9b9a82b2bdf05b6e0e2746571b785a1503d6e
Alberto Ochoa <ochoapublic@gmail.com>
2018-06-28 18:51:46 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,9 +8,9 @@ class CashRegister
     @items = {}
   end
 
-  def add_item(name, price)
+  def add_item(name, price, quantity = 1)
     @items[name] = price
-    @total += price
+    @total += price * quantity
   end
 
 
